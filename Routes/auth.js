805a64a72217fb0c1b3c77ea7debea50412fe08c
R4idenW4sT4ken <143252170+R4idenW4sT4ken@users.noreply.github.com>
2026-05-10 12:05:59 +0200

    First Push

---
+++ Routes/auth.js
@@ -0,0 +1,29 @@
+/*
+
+Role 2: Backend Developer
+Pages/Files:
+
+routes/auth.js
+routes/events.js
+routes/bookings.js
+middleware/auth.js
+controllers/ (if using MVC)
+Example: routes/auth.js
+
+*/
+
+
+// routes/auth.js
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+
+// Login route
+router.get('/login', authController.showLogin);
+router.post('/login', authController.login);
+
+// Register route
+router.get('/register', authController.showRegister);
+router.post('/register', authController.register);
+
+module.exports = router;
